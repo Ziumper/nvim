89c38a5dba80cd17f638278fae25bbfacc4b8bda
Ziumper <ziumper@gmail.com>
2025-12-23 12:12:58 +0100

    Add readme instructions

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Adds following:
 - phpactor support
 - showing numbers in nvim 
+- showing diagnostics errors with `CTRL+W d`
+- limit tab to 2 space characters instead 4
 
 ## Installation 
 
--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 vim.lsp.enable('phpactor')
 vim.opt.number = true
+vim.opt.tabstop = 2     -- Number of spaces a tab counts for
+vim.opt.shiftwidth = 2  -- Size of an indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.softtabstop = 2 -- Number of spaces a tab counts for when editing
